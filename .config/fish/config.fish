21d562bf98868f8a73fe27d1525f9e2205f493d3
Lynder063 <lynderjekokot@gmail.com>
2024-11-30 20:13:24 +0100

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -g fish_greeting
+
+alias vim="nvim"
+
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
