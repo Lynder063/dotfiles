930b718eecc363ba664f23a41b15017fd42df4b1
Lynder063 <lynderjekokot@gmail.com>
2024-12-01 19:44:46 +0100

    big update

--- .config/hypr/xdg-portal-hyprland.sh
+++ .config/hypr/xdg-portal-hyprland.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# For manually starting xdg-desktop-portal-hyprland
-
 sleep 1
-killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-wlr
-killall xdg-desktop-portal-gnome
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-sleep 1
 /usr/lib/xdg-desktop-portal-hyprland &
-/usr/libexec/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
-/usr/libexec/xdg-desktop-portal &
+
